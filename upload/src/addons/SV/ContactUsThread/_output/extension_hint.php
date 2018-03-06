5a31fa92a802ee2da48b96c8b97d12ce7236e7d7
Xon <tazipper1@hotmail.com>
2018-03-06 22:02:27 +0800

    Switch from _data => _output

---
+++ upload/src/addons/SV/ContactUsThread/_output/extension_hint.php
@@ -0,0 +1,24 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace SV\ContactUsThread\XF\Entity
+{
+	class XFCP_User extends \XF\Entity\User {}
+}
+
+namespace SV\ContactUsThread\XF\Pub\Controller
+{
+	class XFCP_Misc extends \XF\Pub\Controller\Misc {}
+}
+
+namespace SV\ContactUsThread\XF\Service
+{
+	class XFCP_Contact extends \XF\Service\Contact {}
+}
+
+namespace SV\ContactUsThread\XF\Validator
+{
+	class XFCP_Username extends \XF\Validator\Username {}
+}
