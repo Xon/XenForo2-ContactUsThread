7176df885bd6bc97362680a1b138359d4ab41683
Xon <tazipper1@hotmail.com>
2018-03-07 05:10:53 +0800

    Implement per-contact form rate limiting

--- upload/src/addons/SV/ContactUsThread/XF/Pub/Controller/Misc.php
+++ upload/src/addons/SV/ContactUsThread/XF/Pub/Controller/Misc.php
@@ -3,6 +3,7 @@
 namespace SV\ContactUsThread\XF\Pub\Controller;
 
 use XF\Mvc\ParameterBag;
+use XF\Util\Ip;
 
 class Misc extends XFCP_Misc
 {
@@ -34,5 +35,39 @@ class Misc extends XFCP_Misc
         }
 
         return parent::actionContact();
+    }
+
+    public function assertNotFlooding($action, $floodingLimit = null)
+    {
+        $visitor = \XF::visitor();
+        if ($action === 'contact' && !$visitor->hasPermission('general', 'bypassFloodCheck'))
+        {
+            $contactFloodingLimit = \XF::options()->svContactUsThreadRateLimit;
+            if (!$contactFloodingLimit)
+            {
+                $contactFloodingLimit = $floodingLimit;
+            }
+            $userId = $visitor->user_id;
+            if (!$userId)
+            {
+                // xf_flood_check.user_id is unsigned 32 bits integer.
+                // Use the IP address crc32'ed as a stand-in for the userid to fit into the field.
+                // set the high bit to ensure it is unlikely to cause a collision with a valid user
+                $binaryIp = Ip::convertIpStringToBinary($this->app->request()->getIp());
+                $userId = crc32($binaryIp) | (1 << 31);
+            }
+
+            /** @var \XF\Service\FloodCheck $floodChecker */
+            $floodChecker = $this->service('XF:FloodCheck');
+            $timeRemaining = $floodChecker->checkFlooding($action, $userId, $contactFloodingLimit);
+            if ($timeRemaining)
+            {
+                throw $this->exception($this->responseFlooding($timeRemaining));
+            }
+
+            return;
+        }
+
+        parent::assertNotFlooding($action, $floodingLimit);
     }
 }
