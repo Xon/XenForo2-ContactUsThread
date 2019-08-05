836c5c4f52cc8b478952fb0143348043a848d55b
Xon <tazipper1@hotmail.com>
2019-08-05 23:59:53 +0800

    Restructure service to be more extendable

--- upload/src/addons/SV/ContactUsThread/XF/Service/Contact.php
+++ upload/src/addons/SV/ContactUsThread/XF/Service/Contact.php
@@ -101,13 +101,13 @@ class Contact extends XFCP_Contact
     public function send()
     {
         $options = $this->app->options();
-        if ($options->svContactUsDiscardDiscourage && $this->isDiscouraged)
+        if (!empty($options->svContactUsDiscardDiscourage) && $this->isDiscouraged)
         {
             return;
         }
         else if ($this->fromEmail)
         {
-            if ($options->svContactUsSilentDiscardBannedEmails)
+            if (!empty($options->svContactUsSilentDiscardBannedEmails))
             {
                 /** @var \XF\Validator\Email $validator */
                 $validator = $this->app->validator('Email');
@@ -141,87 +141,103 @@ class Contact extends XFCP_Contact
         parent::send();
 
         $options = $this->app->options();
+        if (empty($options->svContactUsNode))
+        {
+            return;
+        }
         /** @var \XF\Entity\Forum $forum */
         $forum = $this->em()->find('XF:Forum', $options->svContactUsNode);
 
         if ($forum)
         {
-            $input = [
-                'username' => $this->fromName,
-                'email'    => $this->fromEmail,
-                'subject'  => $this->subject,
-                'message'  => $this->message,
-                'ip'       => $this->fromIp,
-            ];
+            $this->svPostThreadToForum($forum);
+        }
+    }
 
-            $spamTriggerLogDays = intval($options->svContactUsSpamTriggerLogDays);
-            $spamTriggerLogLimit = intval($options->svContactUsSpamTriggerLogLimit);
+    protected function getThreadPhraseInputs(/** @noinspection PhpUnusedParameterInspection */ \XF\Entity\Forum  $forum, \XF\Entity\User  $user)
+    {
+        return [
+            'username'          => $this->fromName,
+            'email'             => $this->fromEmail,
+            'subject'           => $this->subject,
+            'message'           => $this->message,
+            'ip'                => $this->fromIp,
+            'spam_trigger_logs' => $this->getSpamTriggerLogs(),
+        ];
+    }
 
-            if ($spamTriggerLogDays && $spamTriggerLogLimit)
-            {
-                $binaryIp = Ip::convertIpStringToBinary($this->fromIp);
+    protected function getSpamTriggerLogs()
+    {
+        $options = $this->app->options();
 
-                $date = \XF::$time - ($spamTriggerLogDays * 86400);
-                $finder = $this->finder('XF:SpamTriggerLog')
-                               ->with('User')
-                               ->setDefaultOrder('log_date', 'DESC')
-                               ->where('content_type', '=', 'user')
-                               ->where('log_date', '>', $date);
+        $spamTriggerLogDays = intval($options->svContactUsSpamTriggerLogDays);
+        $spamTriggerLogLimit = intval($options->svContactUsSpamTriggerLogLimit);
 
-                $orConditions = [];
-                $orConditions[] = ['User.email', '=', $this->fromEmail];
-                $orConditions[] = ['details', 'like', '%' . $this->fromEmail . '%'];
+        if (!$spamTriggerLogDays || !$spamTriggerLogLimit)
+        {
+            return '';
+        }
+        $binaryIp = Ip::convertIpStringToBinary($this->fromIp);
 
-                if ($binaryIp)
-                {
-                    $orConditions[] = ['ip_address', '=', $binaryIp];
-                }
+        $date = \XF::$time - ($spamTriggerLogDays * 86400);
+        $finder = $this->finder('XF:SpamTriggerLog')
+                       ->with('User')
+                       ->setDefaultOrder('log_date', 'DESC')
+                       ->where('content_type', '=', 'user')
+                       ->where('log_date', '>', $date);
 
-                $finder->whereOr($orConditions);
+        $orConditions = [];
+        $orConditions[] = ['User.email', '=', $this->fromEmail];
+        $orConditions[] = ['details', 'like', '%' . $this->fromEmail . '%'];
 
-                /** @var \XF\Entity\SpamTriggerLog[] $logs */
-                $logs = $finder->fetch()->toArray();
-                $input['spam_trigger_logs'] = $this->_formatLogsForDisplay($logs);
-            }
-            else
-            {
-                $input['spam_trigger_logs'] = '';
-            }
+        if ($binaryIp)
+        {
+            $orConditions[] = ['ip_address', '=', $binaryIp];
+        }
 
+        $finder->whereOr($orConditions);
+
+        /** @var \XF\Entity\SpamTriggerLog[] $logs */
+        $logs = $finder->fetch()->toArray();
+
+        return $this->_formatLogsForDisplay($logs);
+    }
+
+    protected function svPostThreadToForum(\XF\Entity\Forum  $forum)
+    {
+        if ($this->fromUser)
+        {
+            $user = $this->fromUser;
+            $phrase = 'ContactUs_Message_User';
+        }
+        else
+        {
             /** @var \XF\Repository\User $userRepo */
             $userRepo = $this->repository('XF:User');
-
-            $title = $input['subject'];
-            if ($this->fromUser)
-            {
-                $user = $this->fromUser;
-
-                $message = \XF::phrase('ContactUs_Message_User', $input)->render('raw');
-            }
-            else
-            {
-                $username = $this->fromName;
-                $user = $userRepo->getGuestUser($username);
-
-                $message = \XF::phrase('ContactUs_Message_Guest', $input)->render('raw');
-            }
-
-            $creator = \XF::asVisitor($user, function () use ($forum, $title, $message) {
-                /** @var \XF\Service\Thread\Creator $creator */
-                $creator = $this->service('XF:Thread\Creator', $forum);
-                $creator->setPerformValidations(false);
-                $creator->setContent($title, $message);
-                $defaultPrefix = isset($forum->sv_default_prefix_ids) ? $forum->sv_default_prefix_ids : $forum->default_prefix_id;
-                if ($defaultPrefix)
-                {
-                    $creator->setPrefix($defaultPrefix);
-                }
-                $creator->save();
-
-                return $creator;
-            });
-            $creator->sendNotifications();
+            $user = $userRepo->getGuestUser($this->fromName);
+            $phrase = 'ContactUs_Message_Guest';
         }
+        $input = $this->getThreadPhraseInputs($forum, $user);
+
+        $title = $input['subject'];
+        $message = \XF::phrase($phrase, $input)->render('raw');
+
+        $creator = \XF::asVisitor($user, function () use ($forum, $title, $message) {
+            /** @var \XF\Service\Thread\Creator $creator */
+            $creator = $this->service('XF:Thread\Creator', $forum);
+            $creator->setPerformValidations(false);
+            $creator->setContent($title, $message);
+            $defaultPrefix = isset($forum->sv_default_prefix_ids) ? $forum->sv_default_prefix_ids : $forum->default_prefix_id;
+            if ($defaultPrefix)
+            {
+                $creator->setPrefix($defaultPrefix);
+            }
+            $creator->save();
+
+            return $creator;
+        });
+
+        $creator->sendNotifications();
     }
 
     /**
